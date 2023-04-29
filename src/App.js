a9c6cd2e96145113b61ef5e58b4e1057cb5404fa
yosaddis <60548252+yosaddis@users.noreply.github.com>
2023-04-29 22:03:04 +0300

    Add Routing and styling

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// prettier-ignore
+import Nav from './components/Nav';
+import Rockets from './components/Rockets';
+import Missions from './components/Missions';
+import Profile from './components/Profile';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router className="App">
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Rockets />} />
+          <Route path="/missions" element={<Missions />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/*" element={<h1>Don&apos;t exist</h1>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
